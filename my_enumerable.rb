231e6f95d4aa0ed1f6fa10cde5fab96cc884a16c
otegaa <ekiugbotega@gmail.com>
2023-06-16 13:28:59 +0200

    Fix name of file to snake case

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,18 +1,17 @@
 module MyEnumerable
-    def all?(&block)
-      each { |element| return false unless yield(element) }
-      true
-    end
-  
-    def any?(&block)
-      each { |element| return true if yield(element) }
-      false
-    end
-  
-    def filter(&block)
-      result = []
-      each { |element| result << element if yield(element) }
-      result
-    end
+  def all?()
+    each { |element| return false unless yield(element) }
+    true
   end
-  
+
+  def any?()
+    each { |element| return true if yield(element) }
+    false
+  end
+
+  def filter()
+    result = []
+    each { |element| result << element if yield(element) }
+    result
+  end
+end
